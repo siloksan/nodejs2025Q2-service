6386eb3b022debe367ad19661f53d826e278745d
siloksan <tutel07@gmail.com>
2025-01-01 15:05:33 +0300

    refactor: replace the custom validator with a custom one

--- src/common/validators/uuid4-validator.ts
+++
@@ -1,7 +0,0 @@
-import { IsUUID } from 'class-validator';
-import { VALIDATE_MESSAGE } from '../constants';
-
-export class IdValidator {
-  @IsUUID('4', { message: VALIDATE_MESSAGE.INVALID_UUD4 })
-  id: string;
-}
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,10 +6,10 @@ import {
   Param,
   Delete,
   Put,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { IdValidator } from 'src/common/validators';
 import { UpdateUserDto } from './dto/update-password.dto';
 
 @Controller('users')
@@ -27,17 +27,20 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param() { id }: IdValidator) {
+  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.usersService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param() { id }: IdValidator, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param() { id }: IdValidator) {
+  remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.usersService.remove(id);
   }
 }

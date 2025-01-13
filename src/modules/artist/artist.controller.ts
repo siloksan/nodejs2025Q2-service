d5ddf5858c90508d92b251fb082b22f37a458005
siloksan <tutel07@gmail.com>
2025-01-13 21:30:52 +0300

    refactor: artist module

--- src/modules/artist/artist.controller.ts
+++ src/modules/artist/artist.controller.ts
@@ -5,12 +5,11 @@ import {
   Body,
   Param,
   Delete,
-  HttpException,
   ParseUUIDPipe,
   Put,
   HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
-import { CODE_STATUS, ERROR_MESSAGE } from 'src/common/constants';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
@@ -20,58 +19,31 @@ export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistService.create(createArtistDto);
+  async create(@Body() createArtistDto: CreateArtistDto) {
+    return await this.artistService.create(createArtistDto);
   }
 
   @Get()
-  findAll() {
-    return this.artistService.findAll();
+  async findAll() {
+    return await this.artistService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
-    const result = this.artistService.findOne(id);
-
-    if ('status' in result) {
-      throw new HttpException(
-        ERROR_MESSAGE[result.status]('Artist', id),
-        result.status,
-      );
-    }
-
-    return result;
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.artistService.findOne(id);
   }
 
   @Put(':id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateArtistDto: UpdateArtistDto,
   ) {
-    const result = this.artistService.update(id, updateArtistDto);
-
-    if ('status' in result) {
-      throw new HttpException(
-        ERROR_MESSAGE[result.status]('Artist', id),
-        result.status,
-      );
-    }
-
-    return result;
+    await this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
-  @HttpCode(CODE_STATUS.NO_CONTENT)
-  remove(@Param('id', new ParseUUIDPipe()) id: string) {
-    const result = this.artistService.remove(id);
-
-    if (!result) return;
-
-    if ('status' in result) {
-      throw new HttpException(
-        ERROR_MESSAGE[result.status]('Artist', id),
-        result.status,
-      );
-    }
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.artistService.remove(id);
   }
 }
--- src/modules/artist/artist.service.ts
+++ src/modules/artist/artist.service.ts
@@ -1,66 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { ArtistEntity } from './entities/artist.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { DataBase } from 'src/database/in-memory-db/database.service';
-import { CODE_STATUS } from 'src/common/constants';
+import { CODE_STATUS, ERROR_MESSAGE } from 'src/common/constants';
 import { ENTITIES_NAME } from 'src/common/constants/entities-name';
+import { PrismaService } from 'src/database/prisma-module/prisma.service';
 
 @Injectable()
 export class ArtistService {
   private readonly artists: Map<string, ArtistEntity>;
-  constructor(private readonly db: DataBase) {
+  constructor(
+    private readonly db: DataBase,
+    private readonly prisma: PrismaService,
+  ) {
     this.artists = db.artists;
   }
 
-  create(createArtistDto: CreateArtistDto) {
-    const id = uuidv4();
-    const newArtist: ArtistEntity = {
-      id,
-      name: createArtistDto.name,
-      grammy: createArtistDto.grammy,
-    };
-
-    this.artists.set(id, newArtist);
+  async create(createArtistDto: CreateArtistDto) {
+    const newArtist = await this.prisma.artist.create({
+      data: createArtistDto,
+    });
 
     return new ArtistEntity(newArtist);
   }
 
-  findAll() {
-    return Array.from(this.artists.values()).map(
-      (artist) => new ArtistEntity(artist),
-    );
+  async findAll() {
+    return await this.prisma.artist.findMany();
   }
 
-  findOne(id: string) {
-    const artist = this.artists.get(id);
+  async findOne(id: string) {
+    const artist = await this.prisma.artist.findUnique({ where: { id } });
 
     if (!artist) {
-      return { status: CODE_STATUS.NOT_FOUND };
+      throw new HttpException(
+        ERROR_MESSAGE[HttpStatus.NOT_FOUND]('Artist', id),
+        HttpStatus.NOT_FOUND,
+      );
     }
 
     return new ArtistEntity(artist);
   }
 
-  update(id: string, updateArtistDto: UpdateArtistDto) {
-    const artist = this.artists.get(id);
+  async update(id: string, updateArtistDto: UpdateArtistDto) {
+    try {
+      const updatedArtist = await this.prisma.artist.update({
+        where: { id },
+        data: updateArtistDto,
+      });
 
-    if (!artist) {
-      return { status: CODE_STATUS.NOT_FOUND };
+      return new ArtistEntity(updatedArtist);
+    } catch (error) {
+      throw new HttpException(
+        ERROR_MESSAGE[HttpStatus.NOT_FOUND]('Artist', id),
+        HttpStatus.NOT_FOUND,
+      );
     }
-
-    artist.name = updateArtistDto.name;
-    artist.grammy = updateArtistDto.grammy;
-
-    return new ArtistEntity(artist);
   }
 
-  remove(id: string) {
-    if (!this.db.checkEntityExists(ENTITIES_NAME.ARTISTS, id)) {
-      return { status: CODE_STATUS.NOT_FOUND };
+  async remove(id: string) {
+    try {
+      await this.prisma.artist.delete({ where: { id } });
+    } catch {
+      throw new HttpException(
+        ERROR_MESSAGE[HttpStatus.NOT_FOUND]('Artist', id),
+        HttpStatus.NOT_FOUND,
+      );
     }
-
-    this.db.removeEntity(ENTITIES_NAME.ARTISTS, id);
   }
 }

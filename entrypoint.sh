707942241c557bb919032a1e03c20ac6a3631b94
siloksan <tutel07@gmail.com>
2025-01-17 10:38:34 +0300

    fix: entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# This script is helpful when deploying applications in a containerized environment,
-# ensuring that the application doesn't attempt to start before the database is available
-# and the migrations have been executed.
-
-echo "Waiting for Postgres..."
-until nc -z -v -w30 "$POSTGRES_HOST" "$POSTGRES_PORT";
-  do
-  echo "Waiting for database connection on port $POSTGRES_PORT..."
-  sleep 1
-done
-
 echo "Postgres is ready!"
 
 echo "Running migrations..."
